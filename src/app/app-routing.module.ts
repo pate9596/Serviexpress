c2fc488b7105d7b40cb2f8cd1c6b8ef97113379a
josue <thenashrem@gmail.com>
2023-10-30 00:42:32 -0300

    local

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { AuthGuard } from './auth.guard';
 const routes: Routes = [
   {
     path: 'home',
@@ -11,6 +11,12 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'historial',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./historial/historial.module').then( m => m.HistorialPageModule)
+  },
+
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef ,ViewChild} from '@angular/core';
 import { NavigationExtras,Router } from '@angular/router';
-
+import { AutentificarService } from '../Servicios/autentificar.service';
+import { IonModal, IonCard } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonModal)modal!: IonModal;
+  @ViewChild(IonCard, { read: ElementRef }) card!: ElementRef<HTMLIonCardElement>;
+  passwordVisible: boolean = false;
+  password: string = '';
+  isRotated = false;
+  constructor(private router: Router, private auth: AutentificarService) { }
+  public mensaje = ""
+  public alertaButtons = ['OK'];
+  redirectToInicio(){
+    this.router.navigate(['inicio'])
+  }
+  user = {
+    usuario: "",
+    password: ""
+  }
+  informacionUsuario(){
+    this.auth.login(this.user.usuario, this.user.password).then(()=> {
+      if (this.auth.autentificado){
+        let navigationExtras: NavigationExtras ={
+          state: { user: this.user}
+        }
+        this.router.navigate(['/historial'], navigationExtras);
+      } 
+    });
+  }
 
-  constructor() { }
+  Consola(){
+    console.log(this.user);
+    if (this.user.usuario != "" && this.user.password != ""){
+      this.mensaje = "";
+    } else{
+      this.mensaje = ""
+    }
+  }
 
+  Salir(){
+    this.modal.dismiss(null,'Salir');
+  }
+  confirmarUsuario(){
+    this.auth.register(this.user.usuario, this.user.password);
+    this.modal.dismiss(this.user.usuario, 'confirmarUsuario');
+  }
 }
